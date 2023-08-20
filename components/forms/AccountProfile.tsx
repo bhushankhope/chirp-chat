58a07f3a983257ba2effb6a54b88150f93da136c
bhushankhope <bhushankhope@gmail.com>
2023-08-20 00:28:39 -0400

    added profile update logic

--- components/forms/AccountProfile.tsx
+++ components/forms/AccountProfile.tsx
@@ -17,6 +17,8 @@ import Image from "next/image";
 import { ChangeEvent, useState } from "react";
 import { isBase64Image } from "@/lib/utils";
 import { useUploadThing } from "@/lib/uploadthing";
+import { updateUser } from "@/lib/actions/user.actions";
+import { usePathname, useRouter } from "next/navigation";
 
 interface Props {
   user: {
@@ -32,6 +34,9 @@ interface Props {
 const AccountProfile = ({ user, btnTitle }: Props) => {
   const [files, setFiles] = useState<File[]>([]);
   const { startUpload } = useUploadThing("media");
+  const router = useRouter();
+  const pathname = usePathname();
+
   const form = useForm({
     resolver: zodResolver(UserValidation),
     defaultValues: {
@@ -64,13 +69,26 @@ const AccountProfile = ({ user, btnTitle }: Props) => {
     const blob = values.profile_photo;
     const hasImageChanged = isBase64Image(blob);
     if (hasImageChanged) {
-      const imgRes = await startUpload(files)
+      const imgRes = await startUpload(files);
       if (imgRes && imgRes[0].url) {
-        values.profile_photo = imgRes[0].url
+        values.profile_photo = imgRes[0].url;
       }
     }
-    // todo: update user profile
-  }
+    await updateUser({
+      userId: user.id,
+      username: values.username,
+      name: values.name,
+      bio: values.bio,
+      image: values.profile_photo,
+      path: pathname,
+    });
+
+    if (pathname === "/profile/edit") {
+      router.back();
+    } else {
+      router.push('/');
+    }
+  };
   return (
     <Form {...form}>
       <form
