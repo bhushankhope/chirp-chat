7470c4339dac94d806efb00fea1e734ead6faf29
bhushankhope <bhushankhope@gmail.com>
2023-08-24 00:36:54 -0400

    added activity functionality

--- app/(root)/activity/page.tsx
+++ app/(root)/activity/page.tsx
@@ -1,11 +1,56 @@
-import React from 'react'
+import ProfileHeader from "@/components/shared/ProfileHeader";
+import ThreadsTab from "@/components/shared/ThreadsTab";
+import { Tabs, TabsList, TabsContent, TabsTrigger } from "@/components/ui/tabs";
+import UserCard from "@/components/UserCard";
+import { profileTabs } from "@/constants";
+import { fetchUser, fetchUsers, getActivity } from "@/lib/actions/user.actions";
+import { currentUser } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
+import { redirect } from "next/navigation";
 
-const Page = async () => {
+async function Page() {
+  const user = await currentUser();
+  if (!user) return null;
+
+  const userInfo = await fetchUser(user.id);
+  if (!userInfo?.onboarded) redirect("/onboarding");
+
+  //getActivity
+  const activities = await getActivity(userInfo._id);
+
   return (
     <section>
-        <h1 className='head-text mb-10'>Activity</h1>
+      <h1 className="head-text mb-10">Activity</h1>
+      <section className="mt-10 flex flex-col gap-5">
+        {activities.length > 0 ? (
+          <>
+            {activities.map((activity) => (
+              <Link key={activity._id} href={`/thread/${activity.parentId}`}>
+                <article className="activity-card">
+                  <Image
+                    src={activity.author.image}
+                    alt={"Profile Picture"}
+                    width={20}
+                    height={20}
+                    className="object-cover rounded-full"
+                  ></Image>
+                  <p className="!text-smapp-regular text-light-1">
+                    <span className="mr-1 text-primary-500">
+                      {activity.author.name}
+                    </span>{" "}
+                    replied to your thread
+                  </p>
+                </article>
+              </Link>
+            ))}
+          </>
+        ) : (
+          <p className="!text-base-regular text-light-3">No Activity</p>
+        )}
+      </section>
     </section>
-  )
+  );
 }
 
-export default Page
+export default Page;
